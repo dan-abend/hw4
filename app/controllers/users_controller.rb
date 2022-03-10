559681e0793800505ee13d955c82bbe5a15c8754
dan-abend <dan.abend@kellogg.northwestern.edu>
2022-03-10 02:53:18 +0000

    picking up from session controller step

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    @user = User
   end
 
   def create
     @user = User.new(params["user"])
+    plain_text_password = params["user"]["password"]
+    @user.password = BCrypt::Password.create(plain_text_password)
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user.id}"
   end
 end
