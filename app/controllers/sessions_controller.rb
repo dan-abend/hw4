1db4391c27da8fc91ab06077f3f834808fae8add
dan-abend <dan.abend@kellogg.northwestern.edu>
2022-03-10 17:02:24 +0000

    at the views > layouts > application stage of welcome user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    enetered_password = params["password"]
+
+    if @user
+      if BCrypt::Password.new(@user.password) == enetered_password
+        session["user_id"] = @user.id
+
+        flash[:notice] = "Welcome!"
+        redirect_to "/places"
+      else 
+        flash[:notice] = "Password is incorrect"
+        redirect_to "/sessions/new"
+      end 
+  else
+      flash[:notice] = "No user with that email address"
+      redirect_to "/sessions/new"
   end
 
   def destroy
